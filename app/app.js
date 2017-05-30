3d17e50f277d8d9a23ce05cdc9ca1a33efdc667b
ALEXEY TYURIN <ALEXGEOLOG@GMAIL.COM>
2017-05-30 22:13:17 +0300

    Second Commit

--- app/app.js
+++ app/app.js
@@ -1,14 +1,30 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+var model = {
+    user: "Aleksey",
+    items: [
+        {action: "Buy Flowers", done: false},
+        {action: "Clean Room", done: false},
+        {action: "Collect Tickets", done: false},
+        {action: "Call Ann", done: false}
+    ]
+};
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+var todoApp = angular.module("todoApp", []);
+
+todoApp.controller("ToDoController", function ($scope) {
+    $scope.todo = model;
+
+    $scope.incompleteCount = function () {
+        var count = 0;
+        angular.forEach($scope.todo.items, function (item) {
+            if(!item.done) count++
+        });
+        return count;
+    }
+
+    $scope.warningLevel = function () {
+        return $scope.incompleteCount() < 3 ? "label-success" : "label-warning";
+    }
+})
